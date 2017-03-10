56f4717224f11704c41b17a48e6ded7a98c215e1
Danetag <arno.tanielian@gmail.com>
2017-03-10 17:10:23 -0500

    fix isDev in cast

--- receiver/js/app/containers/cast/index.js
+++ receiver/js/app/containers/cast/index.js
@@ -40,8 +40,8 @@ class Cast extends Component {
 	constructor(props) {
 		super(props);
 		this.isDev = (process.env.NODE_ENV === 'development');
-		if (this.isDev) window.cast.receiver.logger.setLevelValue(0);
-		if (this.isDev) this.receiverManager = window.cast.receiver.CastReceiverManager.getInstance();
+		if (!this.isDev) window.cast.receiver.logger.setLevelValue(0);
+		if (!this.isDev) this.receiverManager = window.cast.receiver.CastReceiverManager.getInstance();
 		this.messageBus = null;
 	}
 
